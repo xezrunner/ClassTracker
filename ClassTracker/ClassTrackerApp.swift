7e43bed69a1d2546d9487bb86ad405cc13cc80e0
xezrunner <8061077+xezrunner@users.noreply.github.com>
2024-01-21 03:35:31 +0100

    app: grab study classes on init()

--- ClassTracker/ClassTrackerApp.swift
+++ ClassTracker/ClassTrackerApp.swift
@@ -10,23 +10,42 @@ import SwiftData
 
 @main
 struct ClassTrackerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            StudyClass.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-        
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      StudyClass.self,
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
     }
+  }()
+  
+  init() {
+    let context = sharedModelContainer.mainContext
+    let descriptor = FetchDescriptor<StudyClass>()
+    do {
+      let studyClasses = try context.fetch(descriptor)
+      
+#if DEBUG
+      print("[init] \(studyClasses.count) study classes registered:")
+      for studyClass in studyClasses {
+        print("  - name: \"\(studyClass.name)\"  id: \(studyClass.id)")
+      }
+#endif
+    } catch {
+      print("[init] failed to fetch study classes!")
+    }
+    
+    
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
 }
